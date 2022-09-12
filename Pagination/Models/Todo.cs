2873f41a78a08a53958667b336cf3f995e2dfc3d
graj95 <gauravk@DESKTOP-98OT6G3>
2022-09-12 00:13:09 -0700

    Add project files.

---
+++ Pagination/Models/Todo.cs
@@ -0,0 +1,10 @@
+﻿namespace Pagination.Models
+{
+    public class Todo
+    {
+
+        public int Id { get; set; }
+        public string ItemName { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
